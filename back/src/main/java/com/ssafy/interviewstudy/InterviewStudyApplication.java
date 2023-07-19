37815e99965836965c33c34e6275e2d4ea815315
이상재 <tkdwo7699@gmail.com>
2023-07-19 16:22:02 +0900

    init : 스프링 부트 프로젝트 생성

---
+++ back/src/main/java/com/ssafy/interviewstudy/InterviewStudyApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.interviewstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InterviewStudyApplication.class, args);
+    }
+
+}
---
+++ back/src/test/java/com/ssafy/interviewstudy/InterviewStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.interviewstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
