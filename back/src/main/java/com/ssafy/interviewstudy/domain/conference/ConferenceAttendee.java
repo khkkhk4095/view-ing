0fc70360f2af8ca4b65f9a75c0f56d4ecbe646d0
namjin <skawls25@gmail.com>
2023-07-21 15:46:09 +0900

    refactor: conference entity

--- back/src/main/java/com/ssafy/interviewstudy/domain/conference/ConferenceAttendee.java
+++ back/src/main/java/com/ssafy/interviewstudy/domain/conference/ConferenceAttendee.java
@@ -2,17 +2,21 @@ package com.ssafy.interviewstudy.domain.conference;
 
 import com.ssafy.interviewstudy.domain.member.Member;
 import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
 @Entity
-@Data
+@Getter
+@NoArgsConstructor
 @Table(name = "conference_attendee")
 public class ConferenceAttendee {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer conferenceAttendee_id;
+    @Column(name = "conference_attendee_id")
+    private Integer id;
 
     @JoinColumn(name = "conference_id", nullable = false)
     @ManyToOne(fetch = FetchType.LAZY)
--- back/src/main/java/com/ssafy/interviewstudy/domain/conference/ConferenceRoom.java
+++ back/src/main/java/com/ssafy/interviewstudy/domain/conference/ConferenceRoom.java
@@ -2,6 +2,7 @@ package com.ssafy.interviewstudy.domain.conference;
 
 import com.ssafy.interviewstudy.domain.study.Study;
 import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
@@ -9,14 +10,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-@Data
+@Getter
+@NoArgsConstructor
 @Table(name = "conference_room")
 public class ConferenceRoom {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column
-    private Integer conferenceRoom_id;
+    @Column(name="conference_room_id")
+    private Integer id;
 
 
     @JoinColumn(name = "study_id", nullable = false)
@@ -24,11 +26,9 @@ public class ConferenceRoom {
     private Study study;
 
     @Column(nullable = false)
-    LocalDateTime createdAt;
+    @CreatedDate
+    private LocalDateTime createdAt;
 
-    @Column(nullable = true)
-    LocalDateTime closedAt;
-
-    @OneToMany(mappedBy = "conferenceRoom", cascade = CascadeType.ALL)
-    List<ConferenceAttendee> conferenceAttendeeList = new ArrayList<>();
+    @OneToMany(mappedBy = "conferenceRoom", cascade = CascadeType.REMOVE)
+    private List<ConferenceAttendee> conferenceAttendeeList = new ArrayList<>();
 }
