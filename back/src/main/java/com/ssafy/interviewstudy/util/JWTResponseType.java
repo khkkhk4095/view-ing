14c923c8ff4e091bed6a26fca230b552e5a47f80
이상재 <tkdwo7699@gmail.com>
2023-07-23 08:21:03 +0900

    feat : JWT 생성 및 검증 유틸 생성

---
+++ back/src/main/java/com/ssafy/interviewstudy/util/JWTResponseType.java
@@ -0,0 +1,12 @@
+package com.ssafy.interviewstudy.util;
+
+public enum JWTResponseType {
+
+    //jwt 토큰이 만료되었을때
+    TOKEN_EXPIRED,
+    //jwt 토큰이 유효성 검증에서 실패했을때
+    INVALID_TOKEN,
+    //jwt 토큰이 유효함
+    VALID_TOKEN
+
+}
