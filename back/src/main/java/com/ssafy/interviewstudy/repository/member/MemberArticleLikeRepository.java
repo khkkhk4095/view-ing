0d81a071d9ddc0867f04270a88dcf54c940b5a83
이상재 <tkdwo7699@gmail.com>
2023-07-27 08:34:01 +0900

    fix : 좋아요 한 게시글 조회 게시판 별로 메소드 분류

--- back/src/main/java/com/ssafy/interviewstudy/repository/member/MemberArticleLikeRepository.java
+++ back/src/main/java/com/ssafy/interviewstudy/repository/member/MemberArticleLikeRepository.java
@@ -12,7 +12,17 @@ import java.util.List;
 public interface MemberArticleLikeRepository extends JpaRepository<Board,Integer> {
 
     //멤버 ID(PK)로 좋아요한 게시글 가져오기
-    @Query("select a from Board a inner join a.likes l where l.member.id=:id and Type(a) = :boardType")
-    public List<Board> getArticleByMemberId(@Param("id") Integer memberId, @Param("boardType")BoardType boardType);
+
+    //자유게시판에서 좋아요 한 게시글 가져오기
+    @Query("select a from Board a inner join a.likes l where l.member.id=:id and Type(a) = FreeBoard ")
+    public List<Board> getArticleByMemberIdAtFreeBoard(@Param("id") Integer memberId);
+
+    //질문 게시판에서 좋아요 한 게시글 가져오기
+    @Query("select a from Board a inner join a.likes l where l.member.id=:id and Type(a) = QuestionBoard ")
+    public List<Board> getArticleByMemberIdAtQuestionBoard(@Param("id") Integer memberId);
+
+    //면접후기 게시판에서 좋아요 한 게시글 가져오기
+    @Query("select a from Board a inner join a.likes l where l.member.id=:id and Type(a) = InterviewReviewBoard ")
+    public List<Board> getArticleByMemberIdAtInterviewReviewBoard(@Param("id") Integer memberId);
 
 }
