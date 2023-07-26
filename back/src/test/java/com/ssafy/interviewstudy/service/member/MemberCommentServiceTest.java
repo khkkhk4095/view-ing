3d70f5adb407f1692ad53bf240ea358d17db16aa
이상재 <tkdwo7699@gmail.com>
2023-07-27 08:30:24 +0900

    test: 내가 단 댓글 조회 서비스 테스트

--- back/src/test/java/com/ssafy/interviewstudy/service/member/MemberCommentServiceTest.java
+++ back/src/test/java/com/ssafy/interviewstudy/service/member/MemberCommentServiceTest.java
@@ -1,16 +1,11 @@
 package com.ssafy.interviewstudy.service.member;
 
-import com.ssafy.interviewstudy.domain.board.ArticleComment;
-import com.ssafy.interviewstudy.domain.board.Board;
-import com.ssafy.interviewstudy.domain.board.BoardType;
-import com.ssafy.interviewstudy.domain.board.FreeBoard;
+import com.ssafy.interviewstudy.domain.board.*;
 import com.ssafy.interviewstudy.domain.member.Member;
+import com.ssafy.interviewstudy.dto.board.Author;
 import com.ssafy.interviewstudy.dto.board.BoardRequest;
 import com.ssafy.interviewstudy.dto.board.BoardResponse;
-import com.ssafy.interviewstudy.repository.board.ArticleCommentRepository;
-import com.ssafy.interviewstudy.repository.board.BoardRepository;
-import com.ssafy.interviewstudy.repository.board.FreeBoardRepository;
-import com.ssafy.interviewstudy.repository.board.QuestionBoardRepository;
+import com.ssafy.interviewstudy.repository.board.*;
 import com.ssafy.interviewstudy.repository.member.MemberRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -18,11 +13,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @Transactional
 public class MemberCommentServiceTest {
+
+    @PersistenceContext
+    EntityManager entityManager;
 
     @Autowired
     private MemberCommentService memberCommentService;
@@ -40,6 +42,9 @@ public class MemberCommentServiceTest {
     private QuestionBoardRepository questionBoardRepository;
 
     @Autowired
+    private InterviewReviewBoardRepository interviewReviewBoardRepository;
+
+    @Autowired
     private ArticleCommentRepository articleCommentRepository;
 
     @Test
@@ -52,7 +57,11 @@ public class MemberCommentServiceTest {
         //댓글 작성할 멤버 생성
         Member memberB = Member.builder().email("sj_1333@naver.com").nickname("ssafy123").build();
 
+        memberRepository.save(memberA);
+        memberRepository.save(memberB);
+
         //게시글 작성
+        //자유 게시판
         FreeBoard freeBoardA = new FreeBoard();
         freeBoardA.setTitle("ssafy");
         freeBoardA.setContent("내용내용");
@@ -67,6 +76,21 @@ public class MemberCommentServiceTest {
         freeBoardB.setViewCount(0);
         freeBoardRepository.save(freeBoardB);
 
+        //질문 게시판
+        InterviewReviewBoard interviewReviewBoardA = new InterviewReviewBoard();
+        interviewReviewBoardA.setTitle("ssafy");
+        interviewReviewBoardA.setContent("내용내용");
+        interviewReviewBoardA.setAuthor(memberA);
+        interviewReviewBoardA.setViewCount(0);
+        interviewReviewBoardRepository.save(interviewReviewBoardA);
+
+        InterviewReviewBoard interviewReviewBoardB = new InterviewReviewBoard();
+        interviewReviewBoardB.setTitle("ssafy2");
+        interviewReviewBoardB.setContent("내용내용2");
+        interviewReviewBoardB.setAuthor(memberA);
+        interviewReviewBoardB.setViewCount(0);
+        interviewReviewBoardRepository.save(interviewReviewBoardB);
+
         //댓글 작성
         ArticleComment articleCommentA =
                 ArticleComment.builder().author(memberB).content("하하").article(freeBoardA).isDelete(false).build();
@@ -74,13 +98,42 @@ public class MemberCommentServiceTest {
         ArticleComment articleCommentB =
                 ArticleComment.builder().author(memberB).content("히히").article(freeBoardB).isDelete(false).build();
 
+        ArticleComment articleCommentC =
+                ArticleComment.builder().author(memberA).content("히히").article(interviewReviewBoardA).isDelete(false).build();
+
+        ArticleComment articleCommentD =
+                ArticleComment.builder().author(memberA).content("히히").article(interviewReviewBoardB).isDelete(false).build();
+
         articleCommentRepository.save(articleCommentA);
         articleCommentRepository.save(articleCommentB);
+        articleCommentRepository.save(articleCommentC);
+        articleCommentRepository.save(articleCommentD);
+
         //when
+
+        //memberB가 질문게시판에 작성한 댓글을 가져옴
         BoardRequest boardRequest = new BoardRequest();
         boardRequest.setMemberId(memberB.getId());
-        List<BoardResponse> boardList = memberCommentService.getCommentedArticle(boardRequest, BoardType.free_board);
-        System.out.println(boardList);
+        List<BoardResponse> boardList = memberCommentService.getCommentedArticle(boardRequest, BoardType.FreeBoard);
+
+        //memberA가 면접후기 게시판에 작성한 댓글을 가져옴
+        BoardRequest boardRequest2 = new BoardRequest();
+        boardRequest2.setMemberId(memberA.getId());
+        List<BoardResponse> boardList2 = memberCommentService.getCommentedArticle(boardRequest2, BoardType.InterviewReviewBoard);
+
+        //then
+        //memberB가 작성한 댓글에 대한 검증
+        assertThat(boardList)
+                .extracting("id")
+                .contains(freeBoardA.getId(),freeBoardB.getId())
+                .hasSize(2); 
+        
+        //memberA가 작성한 댓글에 대한 검증
+        assertThat(boardList2)
+                .extracting("id")
+                .contains(interviewReviewBoardA.getId(),interviewReviewBoardB.getId())
+                .hasSize(2);
+
     }
 
 }
