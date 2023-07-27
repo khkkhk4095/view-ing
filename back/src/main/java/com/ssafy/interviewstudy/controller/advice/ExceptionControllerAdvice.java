3e96070d94c919d1ad4e5b4990f08ddce7401604
이상재 <tkdwo7699@gmail.com>
2023-07-27 09:36:03 +0900

    fix: ControllerAdvice 예외 처리 범위 수정

--- back/src/main/java/com/ssafy/interviewstudy/controller/advice/ExceptionControllerAdvice.java
+++ back/src/main/java/com/ssafy/interviewstudy/controller/advice/ExceptionControllerAdvice.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@RestControllerAdvice(basePackageClasses = {MessageController.class, CalendarController.class, MemberBookmarkController.class})
+@RestControllerAdvice("com.ssafy.interviewstudy.controller")
 public class ExceptionControllerAdvice {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
