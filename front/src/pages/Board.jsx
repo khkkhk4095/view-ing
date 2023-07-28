f7228255c54791f2b96acc457214a6f244e3c6b3
Ryu Dong Hoon <123hanyang@naver.com>
2023-07-28 14:51:34 +0900

    feat:redux 프로젝트 적용 완료

--- front/src/pages/Board.jsx
+++ front/src/pages/Board.jsx
@@ -1,3 +1,12 @@
+import { useDispatch, useSelector} from "react-redux"
+import { ChangeToggle } from "../modules/UserRedux/Actions";
+
 export default function Board() {
-  return <></>;
+
+  const dispatch = useDispatch()
+  const user = useSelector((state) => state.UserReducer)
+  console.log(user.toggle)
+  return <>
+    <button onClick={() => dispatch(ChangeToggle())}>Toggle</button>    
+  </>;
 }
