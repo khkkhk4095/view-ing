fc472d35140cbda8317f5bdcc5647aee6fa81d56
이상재 <tkdwo7699@gmail.com>
2023-07-31 09:30:58 +0900

    feat: 인증관련 인터셉터 추가

---
+++ back/src/main/java/com/ssafy/interviewstudy/interceptor/auth/MemberStudyArticleInterceptor.java
@@ -0,0 +1,8 @@
+package com.ssafy.interviewstudy.interceptor.auth;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class MemberStudyArticleInterceptor {
+
+}
--- back/src/main/java/com/ssafy/interviewstudy/interceptor/auth/MemberStudyCommentInterceptor.java
+++ back/src/main/java/com/ssafy/interviewstudy/interceptor/auth/MemberStudyCommentInterceptor.java
@@ -17,28 +17,27 @@ import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
 @Component
-public class LeaderInterceptor implements HandlerInterceptor {
+public class MemberStudyCommentInterceptor implements HandlerInterceptor {
 
     private final StudyService studyService;
 
     private final MemberService memberService;
 
     @Autowired
-    public LeaderInterceptor(StudyService studyService, MemberService memberService) {
+    public MemberStudyCommentInterceptor(StudyService studyService, MemberService memberService) {
         this.studyService = studyService;
         this.memberService = memberService;
     }
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-
         //자기가 처리해야 할 부분인지 체크
         if (handler instanceof HandlerMethod) {
             HandlerMethod handlerMethod = (HandlerMethod) handler;
             // 컨트롤러 메서드에 붙은 어노테이션 정보 가져오기
             Authority annotation = handlerMethod.getMethodAnnotation(Authority.class);
             if (annotation != null) {
-                if (annotation.authorityType() != AuthorityType.Leader) return true;
+                if(annotation.authorityType()!= AuthorityType.Member_Study_Comment) return true;
             }
         }
 
@@ -50,43 +49,46 @@ public class LeaderInterceptor implements HandlerInterceptor {
         Object jwtMemberInfoAttribute = request.getAttribute("JWTMemberInfo");
         JWTMemberInfo jwtMemberInfo;
 
-        if (jwtMemberInfoAttribute instanceof JWTMemberInfo) {
+        if(jwtMemberInfoAttribute instanceof JWTMemberInfo){
             jwtMemberInfo = (JWTMemberInfo) jwtMemberInfoAttribute;
-        } else {
-            response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "잘못된 JWT 정보");
+        }
+        else{
+            response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR,"잘못된 JWT 정보");
             return false;
         }
 
         //PathVariable 유효성 검사
-        if (pathVariables.size() != 2) {
-            response.sendError(HttpServletResponse.SC_BAD_REQUEST, "잘못된 Path Variable");
+        if(pathVariables.size()!=2){
+            response.sendError(HttpServletResponse.SC_BAD_REQUEST,"잘못된 Path Variable");
 
             return false;
         }
-        int memberId = pathVariables.get(0);
-        int studyId = pathVariables.get(1);
-
+        int memberId = jwtMemberInfo.getMemberId();
+        int studyId = pathVariables.get(0);
+        int commentId = pathVariables.get(2);
 
         //PathVariable로 멤버 조회
         Member member = memberService.findMemberByMemberId(memberId);
 
-        if (member == null) {
-            response.sendError(HttpServletResponse.SC_BAD_REQUEST, "없는 유저 입니다.");
+        if(member==null){
+            response.sendError(HttpServletResponse.SC_BAD_REQUEST,"없는 유저 입니다.");
             return false;
-        } else {
-            if (jwtMemberInfo.getMemberId() != member.getId()) {
-                response.sendError(HttpServletResponse.SC_BAD_REQUEST, "잘못된 Path Variable");
+        }
+        else{
+            if(jwtMemberInfo.getMemberId()!=member.getId()){
+                response.sendError(HttpServletResponse.SC_BAD_REQUEST,"잘못된 Path Variable");
                 return false;
             }
         }
-        //리더인지 아닌지 체크 서비스로 할듯
-        Boolean isStudyLeader = studyService.checkStudyLeader(studyId,memberId);
-        if(!isStudyLeader){
-            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "리더가 아닙니다.");
+
+        Boolean isStudyMember = studyService.checkStudyMember(memberId,studyId);
+        if(!isStudyMember){
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED,"해당 스터디 유저가 아닙니다");
             return false;
         }
 
-        return true;
+        //해당 스터디 코멘트 조회
 
+        return true;
     }
 }
