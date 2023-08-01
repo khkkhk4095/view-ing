d8978daa2547610536e7f8913d5ca7f6d5b92147
이상재 <tkdwo7699@gmail.com>
2023-08-01 10:30:29 +0900

    Revert "Merge branch 'feature/225-인가_통합_테스팅_및_리팩토링' into 'develop/be'"
    This reverts merge request !76

--- back/src/main/java/com/ssafy/interviewstudy/controller/member/MypageController.java
+++ back/src/main/java/com/ssafy/interviewstudy/controller/member/MypageController.java
@@ -1,14 +1,11 @@
 package com.ssafy.interviewstudy.controller.member;
 
-import com.ssafy.interviewstudy.annotation.Authority;
-import com.ssafy.interviewstudy.annotation.AuthorityType;
 import com.ssafy.interviewstudy.annotation.JWTRequired;
 import com.ssafy.interviewstudy.domain.board.BoardType;
 import com.ssafy.interviewstudy.dto.board.BoardRequest;
 import com.ssafy.interviewstudy.dto.board.BoardResponse;
 import com.ssafy.interviewstudy.service.member.MemberArticleLikeService;
 import com.ssafy.interviewstudy.service.member.MemberCommentService;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,15 +16,18 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequiredArgsConstructor
 public class MypageController {
 
     private MemberCommentService memberCommentService;
 
     private MemberArticleLikeService memberArticleLikeService;
 
-    @JWTRequired(required = true)
-    @Authority(authorityType = AuthorityType.Member)
+    @Autowired
+    public MypageController(MemberCommentService memberCommentService, MemberArticleLikeService memberArticleLikeService) {
+        this.memberCommentService = memberCommentService;
+        this.memberArticleLikeService = memberArticleLikeService;
+    }
+
     @GetMapping(value="/users/{userId}/article",params = "searchType=favor")
     public ResponseEntity<?> getFavorArticleList(@RequestParam("board")BoardType boardType, @PathVariable("userId") Integer memberId){
         BoardRequest boardRequest = new BoardRequest();
@@ -38,8 +38,6 @@ public class MypageController {
     }
 
 
-    @JWTRequired(required = true)
-    @Authority(authorityType = AuthorityType.Member)
     @GetMapping(value="/users/{userId}/article",params = "searchType=comment")
     public ResponseEntity<?> getCommentedArticleList(@RequestParam("board")BoardType boardType, @PathVariable("userId") Integer memberId){
         BoardRequest boardRequest = new BoardRequest();
