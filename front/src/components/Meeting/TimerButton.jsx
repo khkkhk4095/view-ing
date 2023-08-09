3fbd21f88e9649bd9d669ef642bd6c8e3879dc94
namjin <skawls25@gmail.com>
2023-08-09 15:18:25 +0900

    feat: 영상회의 부가기능 구현

--- front/src/components/Meeting/TimerButton.jsx
+++ front/src/components/Meeting/TimerButton.jsx
@@ -1,9 +1,61 @@
+import { useEffect, useRef, useState } from "react";
 import styled from "styled-components";
 
 const Container = styled.div`
   border: 1px solid black;
 `;
 
+const ReadyState = styled.div``;
+const StartState = styled.div``;
+const StopState = styled.div``;
+
 export default function TimerButton() {
-  return <Container>{"타이머"}</Container>;
+  const [timerState, setTimerState] = useState("ready");
+  const [timer, setTimer] = useState(0);
+  const [saveTime, setSaveTime] = useState(0);
+
+  const checkTime = () => {
+    setTimeout(() => {
+      setTimer((prev) => prev + 1);
+      checkTime();
+    }, 1000);
+  };
+
+  const saveCurrentTime = () => {
+    setSaveTime(timer);
+  };
+
+  useEffect(() => {
+    checkTime();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <Container>
+      <ReadyState
+        hidden={timerState !== "ready"}
+        onClick={() => {
+          setTimerState("start");
+          setTimer(0);
+        }}
+      >
+        {"타이머"}
+      </ReadyState>
+      <StartState
+        hidden={timerState !== "start"}
+        onClick={() => {
+          setTimerState("stop");
+          saveCurrentTime();
+        }}
+      >{`${Math.floor(timer / 60)} : ${Math.floor(timer % 60)}`}</StartState>
+      <StopState
+        hidden={timerState !== "stop"}
+        onClick={() => {
+          setTimerState("ready");
+        }}
+      >
+        {`${Math.floor(saveTime / 60)} : ${Math.floor(saveTime % 60)}`}
+      </StopState>
+    </Container>
+  );
 }
--- front/src/pages/MeetingPk.jsx
+++ front/src/pages/MeetingPk.jsx
@@ -1,17 +1,23 @@
 import { styled } from "styled-components";
-import CamBox from "../components/Meeting/CamBox";
 import MeetingFooter from "../components/Meeting/Organisms/MeetingFooter";
 import MeetingMain from "../components/Meeting/Organisms/MeetingMain";
 import MeetingSideBar from "../components/Meeting/Organisms/MeetingSideBar";
+import { useState } from "react";
 
-const Container = styled.div``;
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+`;
 
 const HeaderContainer = styled.div`
+  position: absolute;
   border: 1px solid black;
   display: flex;
   flex-direction: row;
   justify-content: center;
   align-items: center;
+  width: 100%;
+  height: 5%;
 `;
 
 const TitleContainer = styled.div`
@@ -20,8 +26,29 @@ const TitleContainer = styled.div`
 `;
 
 const MainContainer = styled.div`
+  position: absolute;
   display: flex;
   flex-direction: row;
+  width: 100%;
+  height: 90%;
+  top: 5%;
+  bottom: 5%;
+`;
+
+const MeetingMainContainer = styled.div`
+  position: absolute;
+  border: 1px solid black;
+  right: ${(props) => `${props.right}%`};
+  width: 100%;
+  height: 100%;
+`;
+
+const MeetingSideContainer = styled.div`
+  position: absolute;
+  border: 1px solid black;
+  left: 80%;
+  width: 20%;
+  height: 100%;
 `;
 
 const ButtonContainer = styled.div`
@@ -34,8 +61,32 @@ const LayoutButton = styled.button`
   margin: 1px;
 `;
 
+const FooterContainer = styled.div`
+  position: absolute;
+  border: 1px solid black;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  top: 95%;
+  width: 100%;
+  height: 5%;
+`;
+
 export default function MeetingPk() {
+  const [closeSideBar, setCloseSideBar] = useState(false);
+
+  const [option, setOption] = useState("member");
+
+  const changeOption = (value) => {
+    setOption(value);
+  };
+
+  const toggleSideBar = (value) => {
+    setCloseSideBar(value);
+  };
+
   return (
+    // eslint-disable-next-line no-restricted-globals
     <Container>
       <HeaderContainer>
         <TitleContainer>스터디 제목</TitleContainer>
@@ -46,10 +97,23 @@ export default function MeetingPk() {
         </ButtonContainer>
       </HeaderContainer>
       <MainContainer>
-        <MeetingMain></MeetingMain>
-        <MeetingSideBar></MeetingSideBar>
+        <MeetingMainContainer right={closeSideBar ? 0 : 20}>
+          <MeetingMain></MeetingMain>
+        </MeetingMainContainer>
+        <MeetingSideContainer hidden={closeSideBar}>
+          <MeetingSideBar
+            toggleSideBar={toggleSideBar}
+            changeOption={changeOption}
+            option={option}
+          ></MeetingSideBar>
+        </MeetingSideContainer>
       </MainContainer>
-      <MeetingFooter></MeetingFooter>
+      <FooterContainer>
+        <MeetingFooter
+          toggleSideBar={toggleSideBar}
+          changeOption={changeOption}
+        ></MeetingFooter>
+      </FooterContainer>
     </Container>
   );
 }
