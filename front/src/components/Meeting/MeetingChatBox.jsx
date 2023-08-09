5adead7e8c6e8fab3e2b62e6b2bba42b06870d59
namjin <skawls25@gmail.com>
2023-08-09 17:47:19 +0900

    feat: 녹화기능, 채팅기능 이식 완료

--- front/src/components/Meeting/MeetingChatBox.jsx
+++ front/src/components/Meeting/MeetingChatBox.jsx
@@ -1,9 +1,27 @@
 import styled from "styled-components";
+import UserProfile from "../Common/UserProfile";
 
 const Container = styled.div`
   border: 1px solid black;
 `;
 
-export default function MeetingChatBox() {
-  return <Container>{"채팅하나"}</Container>;
+const MessageContainer = styled.div`
+  border: 1px solid black;
+`;
+
+export default function MeetingChatBox({ data }) {
+  return (
+    <Container>
+      {data.sender === "notice" ? (
+        "-알림-"
+      ) : (
+        <UserProfile
+          nickname={JSON.parse(data.sender).clientData.nickname}
+          backgroundcolor={JSON.parse(data.sender).clientData.background}
+          characterimg={JSON.parse(data.sender).clientData.character}
+        ></UserProfile>
+      )}
+      <MessageContainer>{data.message}</MessageContainer>
+    </Container>
+  );
 }
