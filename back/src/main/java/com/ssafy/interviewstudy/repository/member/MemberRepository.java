4977c3e3f2e8446e1b1b97c4a0cd593931f9229d
이상재 <tkdwo7699@gmail.com>
2023-08-13 20:22:49 +0900

    feat: 탈퇴기능에 따른 메소드 변경

--- back/src/main/java/com/ssafy/interviewstudy/repository/member/MemberRepository.java
+++ back/src/main/java/com/ssafy/interviewstudy/repository/member/MemberRepository.java
@@ -1,19 +1,20 @@
 package com.ssafy.interviewstudy.repository.member;
 
 import com.ssafy.interviewstudy.domain.member.Member;
-import com.ssafy.interviewstudy.domain.study.StudyMember;
 import com.ssafy.interviewstudy.support.member.SocialLoginType;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
+import org.springframework.data.jpa.repository.Query;
 
 public interface MemberRepository extends JpaRepository<Member,Integer> {
 
-    Member findMemberBySocialLoginIdAndSocialLoginType(String socialLoginId, SocialLoginType socialLoginType);
+    @Query("select m from Member m Where m.socialLoginId = :socialLoginId and m.socialLoginType = :socialLoginType and m.status='ACTIVE'")
+    Member findMemberBySocialLoginIdAndSocialLoginTypeAndStatusACTIVE(String socialLoginId, SocialLoginType socialLoginType);
 
-    Member findUserByEmail(String email);
+    @Query("select m from Member m Where m.email = :email and m.status='ACTIVE'")
+    Member findUserByEmailAndStatusACTIVE(String email);
 
-    Member findMemberByNickname(String nickname);
+    @Query("select m from Member m Where m.nickname = :nickname and m.status='ACTIVE'")
+    Member findMemberByNicknameAndStatusACTIVE(String nickname);
 
     //디버깅용
     Member findMemberById(Integer memberId);
