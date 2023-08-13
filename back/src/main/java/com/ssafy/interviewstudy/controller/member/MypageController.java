1f4ac66be4fa7abaee5127afaffbdff463928aa6
이상재 <tkdwo7699@gmail.com>
2023-08-13 16:10:03 +0900

    feat: 내가 쓴 글 보기

--- back/src/main/java/com/ssafy/interviewstudy/controller/member/MypageController.java
+++ back/src/main/java/com/ssafy/interviewstudy/controller/member/MypageController.java
@@ -7,12 +7,13 @@ import com.ssafy.interviewstudy.domain.board.BoardType;
 import com.ssafy.interviewstudy.dto.board.BoardRequest;
 import com.ssafy.interviewstudy.dto.board.BoardResponse;
 import com.ssafy.interviewstudy.dto.study.StudyDtoResponse;
-import com.ssafy.interviewstudy.service.member.MemberArticleLikeService;
+import com.ssafy.interviewstudy.service.board.BoardService;
+import com.ssafy.interviewstudy.service.member.MemberArticleService;
 import com.ssafy.interviewstudy.service.member.MemberCommentService;
 import com.ssafy.interviewstudy.service.study.StudyService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Required;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -24,9 +25,24 @@ public class MypageController {
 
     private final MemberCommentService memberCommentService;
 
+    private final BoardService boardService;
+
     private final StudyService studyService;
 
-    private final MemberArticleLikeService memberArticleLikeService;
+    private final MemberArticleService memberArticleService;
+
+    @JWTRequired(required = true)
+    @Authority(authorityType = AuthorityType.Member)
+    @GetMapping(value="/members/{memberId}/article",params = "searchType=write")
+    public ResponseEntity<?> getMyArticleList(@RequestParam("board")BoardType boardType,
+                                              @PathVariable("memberId") Integer memberId,
+                                              Pageable pageable){
+        BoardRequest boardRequest = new BoardRequest();
+        boardRequest.setMemberId(memberId);
+        List<BoardResponse> articleByMemberId = memberArticleService.getArticleByMemberId(boardRequest, boardType, pageable);
+        return ResponseEntity.ok().body(articleByMemberId);
+
+    }
 
     @JWTRequired(required = true)
     @Authority(authorityType = AuthorityType.Member)
@@ -34,7 +50,7 @@ public class MypageController {
     public ResponseEntity<?> getFavorArticleList(@RequestParam("board")BoardType boardType, @PathVariable("memberId") Integer memberId){
         BoardRequest boardRequest = new BoardRequest();
         boardRequest.setMemberId(memberId);
-        List<BoardResponse> boardResponseList = memberArticleLikeService.getLikedArticleByMemberId(boardRequest,boardType);
+        List<BoardResponse> boardResponseList = memberArticleService.getLikedArticleByMemberId(boardRequest,boardType);
         return ResponseEntity.ok().body(boardResponseList);
 
     }
