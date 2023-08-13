cb7c1bba720f4941f3cf2028b3d49a631341016e
Ryu Dong Hoon <123hanyang@naver.com>
2023-08-13 21:30:13 +0900

    feat: 임시커밋

--- front/src/pages/BoardCommon.jsx
+++ front/src/pages/BoardCommon.jsx
@@ -6,6 +6,7 @@ import { useEffect, useState } from "react";
 import { customAxios } from "../modules/Other/Axios/customAxios";
 import MainButton from "../components/Button/MainButton";
 import { useLocation, useNavigate } from "react-router-dom";
+import { BiChevronLeft, BiChevronRight, BiChevronsLeft, BiChevronsRight } from "react-icons/bi";
 
 const Container = styled.div`
   display: flex;
@@ -17,23 +18,48 @@ const Container = styled.div`
 const BottomContainer = styled.div`
   display: flex;
   align-items: center;
-`
+  margin-bottom: 15px;
+`;
 
 const MarginLeft = styled.div`
   margin-left: 20px;
-`
+`;
 
 const PageContainer = styled.div`
   display: flex;
   justify-content: center;
-`
+  align-items: center;
+`;
 
+
+const PageButton = styled.div`
+  padding: 5px;
+  border-radius: 10px;
+  background-color: ${(props) => {
+    if (props.$now === props.$page) {
+      return "var(--gray-200)";
+    } else {
+      return "";
+    }
+  }};
+  &:hover {
+    background-color: var(--gray-200);
+  }
+`;
+
+const ArrowButton = styled.div`
+  padding: 3px;
+  border-radius: 10px;
+  &:hover {
+    background-color: var(--gray-200);
+  }
+`;
 
 export default function BoardCommon() {
   const [data, setData] = useState([]);
-  const [page, setPage] = useState(0)
-  const navigate = useNavigate()
-  const type = useLocation().pathname.split('/')[2]
+  const [page, setPage] = useState(0);
+  const navigate = useNavigate();
+  const type = useLocation().pathname.split("/")[2];
   const param = (type) => {
     if (type === "free") {
       return "general";
@@ -42,30 +68,66 @@ export default function BoardCommon() {
     } else if (type === "question") {
       return "qna";
     }
+  };
+
+  useEffect(() => {
+    setPage(0);
+    customAxios()
+      .get(`boards/${param(type)}?size=5&page=${page}`) // 페이지size, page
+      .then((res) => {
+        setData(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [type]);
+
+  const handlePage = (e) => {
+    setPage(e - 1)
+    customAxios()
+      .get(`boards/${param(type)}?size=5&page=${e-1}`) // 페이지size, page
+      .then((res) => {
+        setData(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }
 
-  useEffect(()=>{
-    setPage(0)
-    customAxios().get(`boards/${param(type)}?size=5&page=${page}`) // 페이지size, page
-    .then((res) => {
-      setData(res.data)
-    })
-    .catch((err) => {
-      console.log(err)
-    })
-  },[type])
   return (
     <Container>
       <BoardNavBar />
-      <ArticleList data={data} width={1000} type={type}/>
+      <ArticleList data={data} width={1000} type={type} />
       <BottomContainer>
         <SearchBoxBoard></SearchBoxBoard>
         <MarginLeft>
-          <MainButton content={"글쓰기"} width={80} height={35} onClick={() => navigate(`/board/write?type=${type}`)}></MainButton>
+          <MainButton
+            content={"글쓰기"}
+            width={80}
+            height={35}
+            onClick={() => navigate(`/board/write?type=${type}`)}
+          ></MainButton>
         </MarginLeft>
       </BottomContainer>
       <PageContainer>
-
+        <ArrowButton>
+          <BiChevronsLeft></BiChevronsLeft>
+        </ArrowButton>
+        <ArrowButton>
+          <BiChevronLeft></BiChevronLeft>
+        </ArrowButton>
+        <PageButton $now={page} $page={1} onClick={handlePage(1)}>
+          1
+        </PageButton>
+        <PageButton $now={page} $page={2} onClick={handlePage(2)}>
+          2
+        </PageButton>
+        <ArrowButton>
+          <BiChevronRight></BiChevronRight>
+        </ArrowButton>
+        <ArrowButton>
+          <BiChevronsRight></BiChevronsRight>
+        </ArrowButton>
       </PageContainer>
     </Container>
   );
